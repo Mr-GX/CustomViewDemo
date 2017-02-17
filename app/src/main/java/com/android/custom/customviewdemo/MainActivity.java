c2debe833562e0d613a2dda56d865f362faebf79
Guo Xin <xin.guo@after615.com>
2017-02-17 18:05:12 +0800

    alerter lib

--- app/src/main/java/com/android/custom/customviewdemo/MainActivity.java
+++ app/src/main/java/com/android/custom/customviewdemo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.android.custom.customviewdemo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import com.tapadoo.alerter.Alerter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void GuideView(View view) {
         startActivity(new Intent(this, GuideViewActivity.class));
+    }
+
+    public void Alerter(View view) {
+        Alerter.create(this)
+                .setTitle("Alert Title")
+                .setText("Alert text...")
+                .setBackgroundColor(R.color.colorAccent)
+                .setIcon(R.drawable.ic_face)
+                .setDuration(1000)
+                .show();
     }
 }
