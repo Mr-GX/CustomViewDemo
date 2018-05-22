0b23575c527c0eea524c38450c95176d6eeab173
guoxin <g71x88@gmail.com>
2018-05-22 16:23:32 +0800

    test

--- app/src/main/java/com/android/custom/customviewdemo/view/DrawView.java
+++ app/src/main/java/com/android/custom/customviewdemo/view/DrawView.java
@@ -1,20 +1,20 @@
 package com.android.custom.customviewdemo.view;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 import com.android.custom.customviewdemo.R;
 
-
 public class DrawView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    Path path = new Path();
 
     public DrawView(Context context) {
         super(context);
@@ -33,21 +33,32 @@ public class DrawView extends View {
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawColor(getResources().getColor(R.color.colorBg));
-        paint.setColor(Color.BLUE);
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        canvas.drawCircle(150, 150, 100, paint);
-        canvas.drawRect(150, 300, 300, 450, paint);
-        canvas.drawRoundRect(150, 500, 300, 650, 50, 50, paint);
+    public void draw(Canvas canvas) {
+        //总绘制包括onDraw、dispatchDraw、onDrawForeground
+        super.draw(canvas);
+    }
 
-        paint.setStyle(Paint.Style.STROKE);
-        path.moveTo(600,600);
-        path.arcTo(600,600,750,750,-180,180,true);
-        path.arcTo(750,600,900,750,-180,180,true);
-        path.lineTo(750,825);
-        path.lineTo(600,675);
-        canvas.drawPath(path,paint);
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //主体绘制
+        super.onDraw(canvas);
+        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher),100,100,paint);
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        //子View绘制
+        super.dispatchDraw(canvas);
+        paint.setColor(Color.RED);
+        canvas.drawCircle(200,100,10,paint);
+        paint.setColor(Color.BLACK);
+        canvas.drawText("1",197,103,paint);
+    }
+
+    @Override
+    public void onDrawForeground(Canvas canvas) {
+        //前景绘制
+        super.onDrawForeground(canvas);
+        canvas.drawColor(Color.parseColor("#88880000"), PorterDuff.Mode.ADD);
     }
 }
